e287572bbb86b85b3d8e45720910e0ff74b257bc
Ninad Baruah <ninad.2314512828@mujonline.edu.in>
2023-11-28 14:57:50 +0000

    Finish crispy forms

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,35 @@
-from django.shortcuts import render , get_object_or_404
+from django.shortcuts import render , get_object_or_404 , redirect
 from django.utils import timezone
 from blog.models import Post
+from blog.forms import CommentForm
 # Create your views here.
 def index(request):
   posts = Post.objects.filter(published_at__lte=timezone.now())
   context = {'posts':posts}
   return render(request,'blog/index.html',context)
 
-def post_detail(request,slug):
-    post = get_object_or_404(Post,slug=slug)
-    context = {'post':post}
-    return render(request,'blog/post-detail.html',context)
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    # comment_form = None  # Initialize the variable outside the conditional blocks
+
+    if request.user.is_active:
+        if request.method == "POST":
+            comment_form = CommentForm(request.POST)
+
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.content_object = post
+                comment.creator = request.user
+                comment.save()
+                return redirect(request.path_info)
+        else:
+            comment_form = CommentForm()  # Initialize comment_form for GET requests
+    else:
+        comment_form = None  # Set comment_form to None if the user is not active
+
+    context = {'post': post, 'comment_form': comment_form}
+    return render(request, 'blog/post-detail.html', context)
+
+
+
